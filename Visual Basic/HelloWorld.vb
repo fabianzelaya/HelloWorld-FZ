9aaff31f5f4c325e364f668d95d52e3d573ea205
Fabian <71942518+fabianzelaya@users.noreply.github.com>
2022-06-13 13:43:54 -0700

    Init

---
+++ Visual Basic/HelloWorld.vb
@@ -0,0 +1,9 @@
+Imports System
+Public Module HelloWorld
+   'This program will print on the console "Hello, World!"
+   Public Sub Main()
+      Console.WriteLine("Hello, World!")
+      Console.ReadKey()
+   End Sub
+End Module
+'FZ
