90b80f971dceccc5afd96b8de4cd5c73e1968b68
Fabian <71942518+fabianzelaya@users.noreply.github.com>
2022-06-13 13:22:08 -0700

    Init

---
+++ C#/HelloWorld.c
@@ -0,0 +1,14 @@
+using system;
+
+namespace HelloWorld
+{
+    class program
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.ReadLine();
+        }
+    }
+}
+// FZ
